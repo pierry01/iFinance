1180410d6b23f58f371dd910c80ff4c1b7ab32e5
Jean Mendonça <pierrybm@gmail.com>
2023-04-07 10:55:19 -0300

    INSTALL TAILWIND

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-nested"),
+    require("postcss-flexbugs-fixes"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+  ],
+};
