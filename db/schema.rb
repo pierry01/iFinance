3ca14533d135a9e4816c64cc255865e4404336f0
Jean Mendonça <pierrybm@gmail.com>
2023-04-08 15:52:58 -0300

    BUILDING DATABASE

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_08_184936) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_08_185146) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bank_accounts", force: :cascade do |t|
+    t.string "name"
+    t.float "amount"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bank_accounts_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "type"
+    t.string "name"
+    t.boolean "done"
+    t.float "amount"
+    t.date "due_date"
+    t.text "description"
+    t.bigint "bank_account_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bank_account_id"], name: "index_transactions_on_bank_account_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_08_184936) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bank_accounts", "users"
+  add_foreign_key "transactions", "bank_accounts"
 end
