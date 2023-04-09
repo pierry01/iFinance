e4bdf1a19bd06d00c54bb0d9f7c5b2df3d086b09
Jean Mendonça <pierrybm@gmail.com>
2023-04-09 12:39:57 -0300

    ADJUST USER QUERY SPEC

--- spec/graphql/queries/user_query_spec.rb
+++ spec/graphql/queries/user_query_spec.rb
@@ -5,9 +5,10 @@ require "rails_helper"
 module Queries
   RSpec.describe(UserQuery, type: :request) do
     let(:user) { create(:user) }
+    let(:headers) { Devise::JWT::TestHelpers.auth_headers({}, user) }
 
-    it "returns User" do
-      post("/graphql", params: { query: })
+    it "returns the CurrentUser" do
+      post("/graphql", params: { query: }, headers:)
 
       json = JSON.parse(response.body)
       data = json["data"]["userQuery"]
@@ -15,12 +16,21 @@ module Queries
       expect(data["fullName"]).to(eq("first_name last_name"))
     end
 
+    it "returns nil" do
+      post("/graphql", params: { query: })
+
+      json = JSON.parse(response.body)
+      data = json["data"]["userQuery"]
+
+      expect(data).to(be_nil)
+    end
+
     private
 
     def query
       <<~GQL
         {
-          userQuery(id: "#{user.id}") {
+          userQuery {
             id
             fullName
           }
