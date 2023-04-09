f06c5a178b655e01d4d45e802c2845fd3774ce4e
Jean Mendonça <pierrybm@gmail.com>
2023-04-09 13:06:33 -0300

    BETTER ROUTES.JSX

--- app/javascript/components/Routes.jsx
+++ app/javascript/components/Routes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, useLocation } from "react-router-dom";
 
 import Home from "components/Home";
 import NetworkError from "components/NetworkError";
@@ -10,38 +9,21 @@ import CreateBankAccount from "components/bank_account/CreateBankAccount";
 
 import auth from "config/auth";
 
-function RequireAuth({ children }) {
-  return auth.isAuthenticated() ? children : <Navigate to="/login" />;
-}
-
 function Router() {
+  const { pathname } = useLocation();
+
+  if (!auth.isAuthenticated() && pathname !== "/login")
+    return <Navigate to="/login" />;
+
   return (
     <Routes>
-      <Route
-        path="/"
-        element={
-          <RequireAuth>
-            <Home />
-          </RequireAuth>
-        }
-      />
-
-      <Route
-        path="create-bank-account"
-        element={
-          <RequireAuth>
-            <CreateBankAccount />
-          </RequireAuth>
-        }
-      />
-
+      <Route path="/" element={<Home />} />
+      <Route path="create-bank-account" element={<CreateBankAccount />} />
       <Route path="login" element={<Login />} />
       <Route path="network-error" element={<NetworkError />} />
       <Route path="*" element={<NoMatch />} />
     </Routes>
   );
 }
-
-RequireAuth.propTypes = { children: PropTypes.node.isRequired };
 
 export default Router;
