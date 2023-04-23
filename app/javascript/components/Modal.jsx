aff40d1918d225646c344d6c8449e1ed71851c5b
Jean Mendonça <pierrybm@gmail.com>
2023-04-23 15:45:45 -0300

    MODAL 3

--- app/javascript/components/Modal.jsx
+++ app/javascript/components/Modal.jsx
@@ -3,21 +3,29 @@ import PropTypes from "prop-types";
 
 function Modal({ title, onClose, children }) {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-      <div className="relative mx-auto my-6 w-auto max-w-3xl">
-        <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
-          <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
-            <h3 className="text-3xl font-semibold">{title}</h3>
+    <>
+      <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
+        <div className="relative mx-auto my-6 w-auto max-w-3xl">
+          <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
+            <div className="flex justify-between gap-8 rounded-t border-b border-solid border-slate-200 p-5">
+              <h3 className="text-base font-medium text-gray-700">{title}</h3>
 
-            <button type="button" onClick={onClose}>
-              X
-            </button>
+              <button
+                type="button"
+                onClick={onClose}
+                className="text-base font-medium text-gray-700"
+              >
+                x
+              </button>
+            </div>
+
+            <div className="relative flex-auto p-6">{children}</div>
           </div>
-
-          {children}
         </div>
       </div>
-    </div>
+
+      <div className="fixed inset-0 z-40 bg-black opacity-25" />
+    </>
   );
 }
 
